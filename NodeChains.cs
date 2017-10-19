2534acee63b5315c21748065ec1be9f95e8ae9d4
Anthony Balletta <anthonyballetta@gmail.com>
2017-10-19 13:24:59 -0400

    Adding NodeChains.cs

---
+++ NodeChains.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DataStructures
+{
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +-------+--------+
+            // |   3   |  null  +
+            // +-------+--------+
+            // Node has value of 3 and a Next pointer of NULL
+            Node first = new Node { Value = 3 };
+
+            Node middle = new Node { Value = 5 };
+
+            first.Next = middle;
+
+            Node last = new Node { Value = 7 };
+
+            middle.Next = last;
+
+            PrintList(first);
+
+            Console.Read();
+        }
+
+        public static void PrintList(Node node)
+        { 
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
